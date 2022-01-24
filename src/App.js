3f628c6a637eb7a2995f6afebb3ef15dffec88ee
lara <lara.qeshta@gmail.com>
2022-01-24 13:18:23 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <div className="container">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+      </div>
+
+
+
+      <div className="wrap">
+
+      <div className="container-tow">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+      </div>
+
+
+      <div className="container-three">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+      </div>
+
+      </div>
+
+
+      <div className="container-four">
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+        <div className="row"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
